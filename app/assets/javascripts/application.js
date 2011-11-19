532c3a7734c9e0af4941523654690c01c068e802
Josh Rendek <josh@bluescripts.net>
2011-11-19 17:49:20 -0500

    csrf fix + ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,7 @@
 //= require story_types
 //= require tasks
 
+$(document).ajaxSend(function(e, xhr, options) {
+  var token = $("meta[name='csrf-token']").attr("content");
+  xhr.setRequestHeader("X-CSRF-Token", token);
+});
