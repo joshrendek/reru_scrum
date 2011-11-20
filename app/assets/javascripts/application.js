e03b88f1e0e4f3a68a44ffc7fb80bef082e6c855
Josh Rendek <josh@bluescripts.net>
2011-11-20 11:26:08 -0500

    pre asset stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,17 +4,9 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
 //= require jquery_ujs
-//= require bootstrap-dropdown
-//= require bootstrap-alerts
-//= require bootstrap-buttons
-//= require comments
-//= require projects
-//= require statuses
 //= require stories
-//= require story_types
-//= require tasks
+
 
 $(document).ajaxSend(function(e, xhr, options) {
   var token = $("meta[name='csrf-token']").attr("content");
